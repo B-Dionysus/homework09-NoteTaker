dc597ba122b18f4d74b7d5611043f75a49d8b9c4
Benjamin Dionysus <benjamin@sixbynine.com>
2020-10-23 20:09:49 -0500

    got the add message to work!

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -1,31 +1,31 @@
 const { fstat } = require("fs");
 const path=require("path");
 const fs=require("fs");
+const dbPath=(path.join(__dirname, "../db/db.json"));
+
+
 
 function apiRoutes(server){
+    // We're getting a POST request from the user on the notes page.
+    // They must be sending us a note!!!
     server.post("/api/notes",function(req,res){
         let newNote=req.body;
-        addToFile(newNote, "../db/db.json");
-        res.send(newNote);
-    })
+        let {addNote}=require("./io.js");
+        addNote(newNote, dbPath);
+        newNote=JSON.stringify(newNote);
+        return res.json(newNote);
+    });
+    // If the user specifies an id, then they are out for blood and we must destroy
+    // one of the notes that we so recently created!
+    // The GET request on the notes page means they want a list of all of the notes
     server.get("/api/notes",function(req,res){
-        return fs.readFile(path.join(__dirname, "../db/db.json"),"utf8", 
+        return fs.readFile(dbPath,"utf8", 
         // If there is an error, log the error
         (err, data)=> err 
         ? console.log("Error in api/notes: "+err) 
         // Otherwise, send the results to the user
         : res.json(JSON.parse(data)));
-    })
-    // server.get("/api/notes",function(req,res){
-    //     fs.readFile(path.join(__dirname, "../db/db.json"),"utf8", 
-    //     function(err, data){
-    //         if(err) console.log(err);
-    //         else{
-    //             console.log(data);
-    //             return res.json(JSON.parse(data));
-    //         }
-    //     });
-    // });
+    });
 }
 
 
